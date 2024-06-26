e4368c4fede1181a40c60f2b6c35aac8730c1cfe
mahendrakr8687 <mahendrakr8687@gmail.com>
2024-06-26 16:48:57 +0530

    3D-image-gallery

---
+++ script.js
@@ -0,0 +1,29 @@
+const ImageElement = document.querySelector(".image-container");
+
+const PrevEle = document.getElementById("prev");
+const nextEle =document.getElementById("next");
+
+let x = 0;
+let timer;
+
+function updateGallery(){
+    ImageElement.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+
+    timer=setTimeout(()=>{
+      x=x+45;  
+      updateGallery();
+    },3000)
+}
+
+updateGallery();
+
+PrevEle.addEventListener("click",()=>{
+    x=x+45;
+    clearTimeout(timer);
+    updateGallery();
+})
+nextEle.addEventListener("click",()=>{
+    x=x-45;
+    clearTimeout(timer);
+    updateGallery();
+})
